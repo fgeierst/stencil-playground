927bf764e1610bd340b15defcf9e544e6449decb
Florian Geierstanger <florian.geierstanger@codecentric.de>
2025-04-14 22:33:32 +0200

    remove chromatic

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,11 +3,10 @@ import type { StorybookConfig } from '@storybook/sveltekit';
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|ts|svelte)'],
 	addons: [
-		'@storybook/addon-essentials',
-		'@storybook/addon-svelte-csf',
-		'@chromatic-com/storybook',
-		'@storybook/experimental-addon-test'
-	],
+        '@storybook/addon-essentials',
+        '@storybook/addon-svelte-csf',
+        '@storybook/experimental-addon-test'
+    ],
 	framework: {
 		name: '@storybook/sveltekit',
 		options: {}
