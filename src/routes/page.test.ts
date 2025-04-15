da07f9c0fa5502737e95ea30f9b8aa8d9a47bb36
Florian Geierstanger <florian.geierstanger@codecentric.de>
2025-04-15 19:43:04 +0200

    cleanup e2e

--- src/routes/page.test.ts
+++ src/routes/page.test.ts
@@ -1,35 +1,29 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
 	await page.goto('/');
+});
+
+test('has title', async ({ page }) => {
 	await expect(page).toHaveTitle(/StencilJS Playground/);
 });
 
 test('mounts webcontainer', async ({ page }) => {
-	await page.goto('/');
-
 	const terminal = page.getByRole('region', { name: 'Terminal' });
-	const serverReadyMsg = terminal.getByText('Server ready on port');
-	await expect(serverReadyMsg).toBeVisible({
-		timeout: 25 * 1000 // time to mount webcomponent
-	});
+	const ready = terminal.getByText('Server ready on port');
 
+	await expect(ready).toBeVisible({ timeout: 25 * 1000 }); // time to boot webcontainer
 	await expect(terminal).toContainText('build finished, watching for changes...');
 
-	const editor = page.getByRole('region', { name: 'Editor' });
-	await expect(editor).toBeVisible();
-
-	const mountedWebComponent = page
+	const previewComponent = page
 		.locator('iframe[title="Preview"]')
 		.contentFrame()
 		.locator('my-greeting')
 		.filter({ hasText: 'Hello, World!' });
-	await expect(mountedWebComponent).toBeVisible();
+	await expect(previewComponent).toBeVisible();
 });
 
 test('make a code change and update the preview', async ({ page }) => {
-	await page.goto('/');
-
 	const ready = page.getByRole('region', { name: 'Terminal' }).getByText('Server ready');
 	await expect(ready).toBeVisible({ timeout: 25 * 1000 });
 
@@ -41,12 +35,14 @@ test('make a code change and update the preview', async ({ page }) => {
 export class MyGreeting {
   render() { return (<div>Hola!</div>); }
 }`);
+
 	const saveButton = page.getByRole('button', { name: 'Save' });
 	await saveButton.click();
-	const mountedWebComponent = page
+
+	const previewComponent = page
 		.locator('iframe[title="Preview"]')
 		.contentFrame()
 		.locator('my-greeting')
 		.filter({ hasText: 'Hola!' });
-	await expect(mountedWebComponent).toBeVisible();
+	await expect(previewComponent).toBeVisible();
 });
