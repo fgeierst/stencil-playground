2676811b4a3ecd375696e02b3c5ed133826a3682
Florian Geierstanger <florian.geierstanger@codecentric.de>
2025-04-15 14:38:26 +0200

    custom ansi colors for light and dark mode

--- src/lib/components/Terminal.svelte
+++ src/lib/components/Terminal.svelte
@@ -24,6 +24,25 @@
 		display: grid;
 	}
 	pre {
+		--ansi-dim-opacity: 1;
+
+		--ansi-black: light-dark(#333333, #1e1e1e);
+		--ansi-red: light-dark(#b04020, #ce9178);
+		--ansi-green: light-dark(#6a7931, #dcdcaa);
+		--ansi-yellow: light-dark(#6a7931, #dcdcaa);
+		--ansi-blue: light-dark(#005aab, #569cd6);
+		--ansi-magenta: light-dark(#8f3c8c, #c586c0);
+		--ansi-cyan: light-dark(#1e776a, #4ec9b0);
+		--ansi-white: light-dark(#555555, #d4d4d4);
+		--ansi-bright-black: light-dark(#666666, #ababab);
+		--ansi-bright-red: light-dark(#b04020, #e0a080);
+		--ansi-bright-green: light-dark(#6a7931, #ecefcc);
+		--ansi-bright-yellow: light-dark(#6a7931, #ecefcc);
+		--ansi-bright-blue: light-dark(#005aab, #9cdcfe);
+		--ansi-bright-magenta: light-dark(#8f3c8c, #e0a0e0);
+		--ansi-bright-cyan: light-dark(#1e776a, #79e8d2);
+		--ansi-bright-white: light-dark(#555555, #ffffff);
+
 		overflow-y: scroll;
 		font-family: monospace;
 		padding: var(--padding);
